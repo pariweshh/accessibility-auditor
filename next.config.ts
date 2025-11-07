0b16f1c5ec8307f97c19b6bbb77f103e766f4176
pariwesh <pariweshtamrakar@gmail.com>
2025-11-07 19:29:31 +1100

    bug fix for chromium error in production

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
-  experimental: {
-    serverComponentsExternalPackages: ["@sparticuz/chromium"],
-  },
 }
 
 export default nextConfig
